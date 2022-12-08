26784ea8977f7fa6bb91a2700a010254b24f9eab
Renato dos Santos Silva <92689991+Renass21@users.noreply.github.com>
2022-12-08 11:43:51 -0300

    teste 3

--- projetojava1/src/cursojava/executavel/PrimeiraClasseJava.java
+++ projetojava1/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,13 +5,9 @@ public class PrimeiraClasseJava {
 	/*Main é o método auto executável Java*/
 	public static void main(String[] args) {
 		/*Escreve texto no JAVA*/
+		
+		/*alteração teste Coomit*/
 		System.out.println("Hello world, my first program in JAVA. ");
 		System.out.println("Java é orientado a objetos");
 		}
 	}
-
- 
-public static Connection getConnection() { 
-	return connection;
-	
-}
