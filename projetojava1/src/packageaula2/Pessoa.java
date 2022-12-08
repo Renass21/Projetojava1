eb6e6180b3990451ea3f3787f4d813b45280ddf9
Renato dos Santos Silva <92689991+Renass21@users.noreply.github.com>
2022-12-08 14:46:23 -0300

    Adicionando campo de Data Nascimento

--- projetojava1/src/packageaula2/Pessoa.java
+++ projetojava1/src/packageaula2/Pessoa.java
@@ -1,5 +1,7 @@
 package packageaula2;
 
+import java.util.Date;
+
 public class Pessoa {
 
 	private String nome;
@@ -7,6 +9,16 @@ public class Pessoa {
 	private String sobrenome;
 	
 	private int idade;
+	
+	private Date dataNascimento;
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 
 	public int getIdade() {
 		return idade;
