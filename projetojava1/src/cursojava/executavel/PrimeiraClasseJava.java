5b15e95eefd1817a64c6105d3e751d5266ceb037
Renato dos Santos Silva <92689991+Renass21@users.noreply.github.com>
2022-12-08 10:49:20 -0300

    Criação do projeto

---
+++ projetojava1/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	/*Main é o método auto executável Java*/
+	public static void main(String[] args) {
+		
+		
+		/*Escreve texto no JAVA*/
+		System.out.println("Hello world, my first program in JAVA. ");
+		System.out.println("Java é orientado a objetos");
+	}
+}
