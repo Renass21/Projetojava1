15ca62f0ad2c97eb4e94053225d5880602b96afb
Renato dos Santos Silva <92689991+Renass21@users.noreply.github.com>
2023-01-31 11:35:56 -0300

    Sistema de cálculo de média de notas com Java.

--- projetojava1/src/cursojava/executavel/PrimeiraClasseJava.java
+++ projetojava1/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,10 +4,77 @@ public class PrimeiraClasseJava {
 
 	/*Main é o método auto executável Java*/
 	public static void main(String[] args) {
-		/*Escreve texto no JAVA*/
+		/*String -  escreve texto, numeros, sem restrição de tamanho*/
 		
-		/*alteração teste Coomit*/
-		System.out.println("Hello world, my first program in JAVA. ");
-		System.out.println("Java é orientado a objetos");
-		}
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		/*int nota1 = 95;
+			int nota2 = 91;
+			int nota3 = 90;
+			int nota4 = 95;
+			int media = 0;
+			
+			media = (nota1 + nota2 + nota3 + nota4) /4;*/
+			
+			/*Condição lógica com if  e else - Aula 1*/
+					
+				/*if (media >= 70) {
+					System.out.println("Parabéns,você foi aprovado: " + media);
+					}else if (media >= 50 && media <= 69) {
+					System.out.println("Aluno em recuperação: " + media);
+					}else {	
+					System.out.println("Aluno reprovado: " + media);
+					}*/
+ 		
+			/*operadores ternários são para micro validações - Aula 2*/
+				
+					/*String saidaResultado = media >=70 ? "Aluno Aprovado" : (media >= 50 && media <= 69) ? "Aluno em recuperação" : "Aluno Reprovado";
+							System.out.println(saidaResultado); */
+			
+			/*Operações lógicas aninhadas - Aula 3*/
+			 		
+					/*if (media >= 50) {
+						if (media >= 70) {
+							if (media > 90) {
+							System.out.println(" Aluno aprovado - P A R A B É N S! ! ! : " + media);
+							}else {	
+							System.out.println("Aluno aprovado! : " + media);
+								}
+							}else {	   
+								System.out.println("Aluno em recuperação! :" + media);
+							}
+						}else {
+								System.out.println("Aluno reprovado!- Se esforçe mais da proxima vez! : " + media); 
+					}*/
+	
+			
+			/* Switch CASE: OPERAÇÕES EXATAS - Aula 4
+					
+			int dia = 1;	
+			switch (dia) {
+				case 1:
+					System.out.println("Domingo");
+					break;
+				case 2:
+					System.out.println("Segunda");
+					break;
+
+				default: System.out.println("Outro dia qualquer");
+					break;
+				} */
+	
+	
+	
+	
+	
+	
+	
 	}
+}
